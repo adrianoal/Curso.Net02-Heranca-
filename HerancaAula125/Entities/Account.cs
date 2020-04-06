2eb857f3d9a2180efac36359b490d364b7fc8203
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-04-06 19:30:31 -0300

    Aula 125 - Herança Upacasting e Downcasting

---
+++ HerancaAula125/Entities/Account.cs
@@ -0,0 +1,35 @@
+﻿namespace HerancaAula125.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; } // protected:  So pode ser alterado por uma subclasse
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double quantia)
+        {
+            Balance -= quantia;
+        }
+
+        public void Deposit(double quantia)
+        {
+
+            Balance += quantia;
+        }
+
+
+
+
+    }
+}
---
+++ HerancaAula125/Entities/SavingAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using HerancaAula125.Entities;
+
+namespace HerancaAula125.Entities
+{
+    class SavingAccount : Account
+    {
+        public double InterestRate { get; set; } // Taxa de juros
+
+        public SavingAccount()
+        {
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interestRate) : base (number, holder, balance)
+        {
+            
+            InterestRate = interestRate;
+        }
+        
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+
+
+    }
+}
