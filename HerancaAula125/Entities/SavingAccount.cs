fd532451d6283f91993fcace1998a1c416fb7905
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-17 11:07:26 -0300

    Classes e metodos selados

--- HerancaAula125/Entities/SavingAccount.cs
+++ HerancaAula125/Entities/SavingAccount.cs
@@ -5,7 +5,8 @@ using System.Globalization;
 
 namespace HerancaAula125.Entities
 {
-    class SavingAccount : Account
+    // sealed: Uso sealed na frente da classe, pq nao que que outras classes herdem dela. (Regra de negocio)
+    sealed class SavingAccount : Account
     {
         public double InterestRate { get; set; } // Taxa de juros
 
@@ -26,14 +27,31 @@ namespace HerancaAula125.Entities
         }
         
         // override: Sign. q estou 'sobrepondo' aproveitando o metodo da superclasse, e implementando na subclasse, conforme abaixo:
+        //public sealed override void Withdraw(double quantia) // sealed: Sign. q este metodo pode ser selado, pq ele e um metodo sobreposto da superclasse, e por questao de regra de negocio, nao quero q outros metodos sejam sobreposto apartir dele
+
+
         public override void Withdraw(double quantia)
         {
             base.Withdraw(quantia); //base: com o uso da palavra base, eu reaproveito o metodo da superclasse, e implemento minha subclasse com o que eu quiser.
-            Balance -= 2.0; 
-            
+            Balance -= 2.0;            
         }
-
-
-
     }
 }
+
+/*
+Para que serve selar(sealed) uma classe ou um método?
+
+   * Segurança: Dependendo das regras do negócio, às vezes é desejável garantir que uma classe não seja herdada, ou que um
+     método não seja sobreposto. (Ex: o Metodo Withdraw da subclasse SavingsAccount).
+
+
+    * Geralmente convém selar métodos sobrepostos, pois sobreposições múltiplas podem ser uma
+      porta de entrada para inconsistências
+
+
+    * Performance: Atributos de tipo de uma classe selada são analisados de forma mais rápida
+      em tempo de execução.
+
+
+    * Exemplo clássico: string
+*/
